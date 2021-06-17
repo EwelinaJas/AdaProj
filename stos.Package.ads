587e0b7e64894e55cf2642f0114ce4f2ded9f072
Ewelina <32543110+EwelinaJas@users.noreply.github.com>
2021-06-17 11:25:52 +0200

    Update stos.Package.ads

--- stos.Package.ads
+++ stos.Package.ads
@@ -1,5 +1,6 @@
+-- potwierdzenie czy bufor jest pelny czy pusty
 package stosPackage is 
- 	type stos is array(integer range<>)of Float; 
+ 	type stos is array(integer range<>)of Float; -- deklaracja typu liczba tutaj float
 	type stosStruktura(pojemnosc:Integer)is 
 		record
 			st:stos(1..pojemnosc);
@@ -7,6 +8,6 @@ package stosPackage is
 	    end record;
 	 procedure wstaw(s: Access stosStruktura; x:in Float);
 	 procedure pobierz(s: Access stosStruktura; x:out Float);
-	 function pelny (s:Access stosStruktura) return boolean; 
-	 function Pusty (s:Access stosStruktura) return boolean;
+	 function pelny (s:Access stosStruktura) return boolean; -- wszystkie miejsca w array zapelnione
+	 function Pusty (s:Access stosStruktura) return boolean; --deklaracja typu kolejki fifo (?) 
  end stosPackage; 
